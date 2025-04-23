6dda01aeeaa90bb8ae6e3bb5a071d6e91d780e60
TheCharBot <charles@lpvondollen.family>
2025-04-22 20:48:04 -0700

    Main.py

--- A Minecraft RPG/Main.py
+++ A Minecraft RPG/Main.py
@@ -32,7 +32,7 @@ def map(x, y, map):
 class Player(pygame.sprite.Sprite):
     def __init__(self):
         pygame.sprite.Sprite.__init__(self)
-        self.speed = 1
+        self.speed = 200
         self.x = 0
         self.y = 0
         self.direction = pygame.Vector2()
@@ -40,13 +40,18 @@ class Player(pygame.sprite.Sprite):
         self.rect = self.image.get_rect(center = (WINDOW_WIDTH/2, WINDOW_HEIGHT/2))
     def update(self, dt):
         keys = pygame.key.get_pressed()
-        self.direction.x = int(keys[pygame.K_d]) - int(keys[pygame.K_a])
-        self.direction.y = int(keys[pygame.K_s]) - int(keys[pygame.K_w])
+        self.direction.x = int(keys[pygame.K_RIGHT]) - int(keys[pygame.K_LEFT])
+        # Calculate vertical direction
+        self.direction.y = int(keys[pygame.K_DOWN]) - int(keys[pygame.K_UP])
         self.direction = (
             self.direction.normalize() if self.direction else self.direction
         )
-        #rectangle hitbox movement calculation
-        self.rect.center += self.direction * self.speed * dt
+        self.rect.x += self.direction.x * self.speed * dt
+        self.rect.y += self.direction.y * self.speed * dt
+        ###  REWORK  ###
+        #self.direction.x = int(keys[pygame.K_a]) - int(keys[pygame.K_d])
+        #self.direction.y = int(keys[pygame.K_w]) - int(keys[pygame.K_s])
+       
 
 all_sprites = pygame.sprite.Group()
 player = Player()
@@ -57,10 +62,13 @@ while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+
     all_sprites.update(dt)
+    
     display_surface.fill("black")
+    
     map(0,0,overworld)
     all_sprites.draw(display_surface)
     
-    pygame.display.flip()
+    pygame.display.update()
 pygame.quit()
