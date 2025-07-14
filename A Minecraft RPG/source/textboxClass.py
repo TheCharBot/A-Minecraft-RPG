c2ccc73e96a49ccf378dd1ab7e7f1e3de91fb0df
TheCharBot <charles@lpvondollen.family>
2025-07-13 18:03:30 -0700

    Delete A Minecraft RPG/source/textboxClass.py

--- A Minecraft RPG/source/textboxClass.py
+++
@@ -1,9 +0,0 @@
-from source.Main import *
-
-pygame.font.init()
-
-class Textbox():
-    def __init__(self):
-        pygame.sprite.Sprite.__init__(self)
-        self.image = pygame.image.load(images.textbox)
-    
